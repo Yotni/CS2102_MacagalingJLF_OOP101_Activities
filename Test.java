888905902370d11a443ac14c7dab7b77219eab3e
JamesLawrence <jameslawrencefmacagaling@gmail.com>
2026-08-28 16:23:37 +0800

    testing

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 class Test{
     static void main(String[] args){
         System.out.println("Coding in Java using VS Code");
+        System.out.println("it is happening naga na ba po");
     }
 }
