5a10620392c0c053aa7637d79d9c4631a9d3eb3f
JamesLawrence <jameslawrencefmacagaling@gmail.com>
2026-09-10 15:34:35 +0800

    Labaratory Activity 1 Add

---
+++ LaboratoryActivity1.java
@@ -0,0 +1,13 @@
+public class LaboratoryActivity1 {
+    static void main(String[] args) {
+
+        System.out.println("==== Menu ====");
+        System.out.printf("%-20s %.2f \n", "1. Burger", 20.00);
+        System.out.printf("%-20s %.2f \n", "2. Pizza Slize", 25.00);
+        System.out.printf("%-20s %.2f \n", "3. Siaomai Rice", 45.00);
+        System.out.printf("%-20s %.2f \n", "4. Spaghetti ", 50.00);
+        System.out.printf("%-20s %.2f \n", "5. Combo Meal(Chicken, Rice, and Egg) ", 65.00);
+
+
+    }
+}
